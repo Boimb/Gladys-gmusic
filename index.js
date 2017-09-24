97dbd34bb4c37f3a2dbb46d406c6f5d250b342fe
Boimb <atboimb@gmail.com>
2017-09-24 16:13:57 +0200

    install & init

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const install = require('./lib/install.js');
 const init = require('./lib/init.js');
 const debug  = require('lib/debug');
 
+gladys.on('ready', function(){
+    init().catch(sails.log.warn);
+});
+
 module.exports = function(sails) {
 
 
