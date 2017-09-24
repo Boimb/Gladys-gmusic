d410e378b548044480be09c0413b6606af5ae704
Boimb <atboimb@gmail.com>
2017-09-24 16:20:37 +0200

    typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const install = require('./lib/install.js');
 const init = require('./lib/init.js');
-const debug  = require('lib/debug');
+const debug  = require('./lib/debug');
 
 gladys.on('ready', function(){
     init().catch(sails.log.warn);
